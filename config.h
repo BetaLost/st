2653e753a19b195315bb21db86d959a0b724a55b
Beta_Lost <42658940+BetaLost@users.noreply.github.com>
2023-03-22 15:48:10 +0400

    Add files via upload

--- config.h
+++ config.h
@@ -96,32 +96,32 @@ unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+	"#272822", // black
+	"#f92672", // red
+	"#a6e22e", // green
+	"#e6db74", // yellow
+	"#66d9ef", // blue
+	"#fd971f", // magenta
+	"#ae81ff", // cyan
+	"#f8f8f0", // white
 
 	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	"#272822", // black
+	"#f92672", // red
+	"#a6e22e", // green
+	"#e6db74", // yellow
+	"#66d9ef", // blue
+	"#fd971f", // magenta
+	"#ae81ff", // cyan
+	"#f8f8f0", // white
 
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
+	"#f8f8f8",
 	"#555555",
-	"gray90", /* default foreground colour */
-	"black", /* default background colour */
+	"#f8f8f0", /* default foreground colour */
+	"#1a1a18", /* default background colour */
 };
 
 
